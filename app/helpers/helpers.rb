a54630b8db16bddda073faa5f6dc1f8ebb42cefb
Daniel Sasser <danielbsasser@gmail.com>
2020-03-28 14:15:09 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
-class Helpers < ActiveRecord::Base
+class Helpers
 
   def self.current_user(session)
-    user = find_by_id(session['user_id'])
+    @user = User.find(session[:user_id])
   end
 
   def self.is_logged_in?
